9978747c78e0c8a8161f71a3cd8dab3cdbc02119
Code Sandbox <hello@codesandbox.io>
2021-05-14 10:13:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.041d1b362fe6f6789224f28bddfc6fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6198491bb8d6563552b92efc9f096a7c",
+    "url": "https://muhi101.github.io/csb-c645l/index.html"
+  },
+  {
+    "revision": "f44b6705fdb18c1bac77",
+    "url": "https://muhi101.github.io/csb-c645l/static/css/2.daec189b.chunk.css"
+  },
+  {
+    "revision": "f44b6705fdb18c1bac77",
+    "url": "https://muhi101.github.io/csb-c645l/static/js/2.ced93049.chunk.js"
+  },
+  {
+    "revision": "d29b44971445a9dfb1abb1a2dbf48868",
+    "url": "https://muhi101.github.io/csb-c645l/static/js/2.ced93049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7cd35b1f7e29e2f6d5",
+    "url": "https://muhi101.github.io/csb-c645l/static/js/main.296a0092.chunk.js"
+  },
+  {
+    "revision": "fe6f49e38524ae21356a",
+    "url": "https://muhi101.github.io/csb-c645l/static/js/runtime-main.a9758c88.js"
+  }
+]);
